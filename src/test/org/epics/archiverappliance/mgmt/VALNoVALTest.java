52e7abe6dc60821cc91212ad5b439fb530622642
Lee, Jeong Han <jeonghan.lee@gmail.com>
2026-09-13 03:12:02 -0700

    Rewrite VALNoVALTest to mgmt BPL HTTP
    Replace the browser flow with direct archivePV calls for a PV and a PV field,
    poll getPVStatus until both are being archived, then confirm data is
    retrievable under the bare name and its .VAL form. Verifies the server
    workflow rather than the browser DOM.

--- src/test/org/epics/archiverappliance/mgmt/VALNoVALTest.java
+++ src/test/org/epics/archiverappliance/mgmt/VALNoVALTest.java
@@ -1,9 +1,9 @@
 package org.epics.archiverappliance.mgmt;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.awaitility.Awaitility;
 import org.epics.archiverappliance.Event;
 import org.epics.archiverappliance.EventStream;
 import org.epics.archiverappliance.SIOCSetup;
@@ -11,142 +11,127 @@ import org.epics.archiverappliance.TomcatSetup;
 import org.epics.archiverappliance.common.TimeUtils;
 import org.epics.archiverappliance.config.ConfigServiceForTests;
 import org.epics.archiverappliance.retrieval.client.RawDataRetrievalAsEventStream;
+import org.epics.archiverappliance.utils.ui.GetUrlContent;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
-import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.time.Instant;
 
 /**
- * A common use case is where we archive the .VAL and ask for data either way.
- * This test archives two PVs; one with a .VAL and one without.
- * We then ask for data in all combinations and make sure we get said data. 
- * @author mshankar
- *
+ * Archive a PV and a PV field (VAL) through the mgmt BPL, replacing the former browser-driven flow,
+ * then confirm data is retrievable under both the bare name and its .VAL form. Verifies the server
+ * workflow rather than the UI.
  */
-@Tag("integration")@Tag("localEpics")
+@Tag("integration")
+@Tag("localEpics")
 public class VALNoVALTest {
-	private static Logger logger = LogManager.getLogger(VALNoVALTest.class.getName());
-	TomcatSetup tomcatSetup = new TomcatSetup();
-	SIOCSetup siocSetup = new SIOCSetup();
-	WebDriver driver;
-	String folderSTS = ConfigServiceForTests.getDefaultShortTermFolder() + File.separator + "reshardSTS";
-	String folderMTS = ConfigServiceForTests.getDefaultPBTestFolder() + File.separator + "reshardMTS";
-	String folderLTS = ConfigServiceForTests.getDefaultPBTestFolder() + File.separator + "reshardLTS";
+    private static Logger logger = LogManager.getLogger(VALNoVALTest.class.getName());
+    private static final String MGMT = "http://localhost:17665/mgmt/bpl/";
+    TomcatSetup tomcatSetup = new TomcatSetup();
+    SIOCSetup siocSetup = new SIOCSetup();
+    String folderSTS = ConfigServiceForTests.getDefaultShortTermFolder() + File.separator + "reshardSTS";
+    String folderMTS = ConfigServiceForTests.getDefaultPBTestFolder() + File.separator + "reshardMTS";
+    String folderLTS = ConfigServiceForTests.getDefaultPBTestFolder() + File.separator + "reshardLTS";
 
-	@BeforeAll
-	public static void setupClass() {
-		WebDriverManager.firefoxdriver().setup();
-	}
+    @BeforeEach
+    public void setUp() throws Exception {
+        System.getProperties().put("ARCHAPPL_SHORT_TERM_FOLDER", folderSTS);
+        System.getProperties().put("ARCHAPPL_MEDIUM_TERM_FOLDER", folderMTS);
+        System.getProperties().put("ARCHAPPL_LONG_TERM_FOLDER", folderLTS);
 
-	@BeforeEach
-	public void setUp() throws Exception {
-		System.getProperties().put("ARCHAPPL_SHORT_TERM_FOLDER", folderSTS);
-		System.getProperties().put("ARCHAPPL_MEDIUM_TERM_FOLDER", folderMTS);
-		System.getProperties().put("ARCHAPPL_LONG_TERM_FOLDER", folderLTS);
-		
-		FileUtils.deleteDirectory(new File(folderSTS));
-		FileUtils.deleteDirectory(new File(folderMTS));
-		FileUtils.deleteDirectory(new File(folderLTS));
+        FileUtils.deleteDirectory(new File(folderSTS));
+        FileUtils.deleteDirectory(new File(folderMTS));
+        FileUtils.deleteDirectory(new File(folderLTS));
 
-		siocSetup.startSIOCWithDefaultDB();
-		tomcatSetup.setUpWebApps(this.getClass().getSimpleName());
-		driver = new FirefoxDriver();
-	}
+        siocSetup.startSIOCWithDefaultDB();
+        tomcatSetup.setUpWebApps(this.getClass().getSimpleName());
+    }
 
-	@AfterEach
-	public void tearDown() throws Exception {
-		driver.quit();
-		tomcatSetup.tearDown();
-		siocSetup.stopSIOC();
+    @AfterEach
+    public void tearDown() throws Exception {
+        tomcatSetup.tearDown();
+        siocSetup.stopSIOC();
 
-		FileUtils.deleteDirectory(new File(folderSTS));
-		FileUtils.deleteDirectory(new File(folderMTS));
-		FileUtils.deleteDirectory(new File(folderLTS));
-}
+        FileUtils.deleteDirectory(new File(folderSTS));
+        FileUtils.deleteDirectory(new File(folderMTS));
+        FileUtils.deleteDirectory(new File(folderLTS));
+    }
 
-	@Test
-	public void testVALNoVALTest() throws Exception {
-		 driver.get("http://localhost:17665/mgmt/ui/index.html");
-		 WebElement pvstextarea = driver.findElement(By.id("archstatpVNames"));
-		 String pvNameToArchive1 = "UnitTestNoNamingConvention:sine";
-		 pvstextarea.sendKeys(pvNameToArchive1);
-		 pvstextarea.sendKeys(Keys.RETURN);
-		 String pvNameToArchive2 = "UnitTestNoNamingConvention:cosine.VAL";
-		 pvstextarea.sendKeys(pvNameToArchive2);
-		 pvstextarea.sendKeys(Keys.RETURN);
-		 
-		 WebElement archiveButton = driver.findElement(By.id("archstatArchive"));
-		 logger.debug("About to submit");
-		 archiveButton.click();
-		 // We have to wait for a few minutes here as it does take a while for the workflow to complete.
-		 Thread.sleep(5*60*1000);
-		 WebElement checkStatusButton = driver.findElement(By.id("archstatCheckStatus"));
-		 checkStatusButton.click();
-		 Thread.sleep(2*1000);
-		 checkArchiveStatus(pvNameToArchive1, 1);
-		 checkArchiveStatus(pvNameToArchive2, 2);
-		 Thread.sleep(60*1000);
-		 testRetrievalCountOnServer(pvNameToArchive1, 55);
-		 testRetrievalCountOnServer(pvNameToArchive1 + ".VAL", 55);
-		 testRetrievalCountOnServer(pvNameToArchive1, 55);
-		 testRetrievalCountOnServer(pvNameToArchive1 + ".VAL", 55);
-	}
+    private static String enc(String pv) {
+        return URLEncoder.encode(pv, StandardCharsets.UTF_8);
+    }
 
-	/**
-	 * Check to see that we get a Being archived status. We take the pvName and also the row in the table we expect the data.
-	 * @param pvNameToArchive
-	 * @param rowNum
-	 */
-	private void checkArchiveStatus(String pvNameToArchive, int rowNum) {
-		{
-			 WebElement statusPVName = driver.findElement(By.cssSelector("#archstatsdiv_table tr:nth-child(" + rowNum + ") td:nth-child(1)"));
-			 String pvNameObtainedFromTable = statusPVName.getText();
-			 Assertions.assertTrue(pvNameToArchive.equals(pvNameObtainedFromTable), "PV Name is not " + pvNameToArchive + "; instead we get " + pvNameObtainedFromTable);
-			 WebElement statusPVStatus = driver.findElement(By.cssSelector("#archstatsdiv_table tr:nth-child(" + rowNum + ") td:nth-child(2)"));
-			 String pvArchiveStatusObtainedFromTable = statusPVStatus.getText();
-			 String expectedPVStatus = "Being archived";
-			 Assertions.assertTrue(expectedPVStatus.equals(pvArchiveStatusObtainedFromTable), "Expecting PV archive status to be " + expectedPVStatus + "; instead it is " + pvArchiveStatusObtainedFromTable);
-		 }
-	}
-	
-	/**
-	 * Get data for the PV from the server and make sure we have some data
-	 * @param pvName
-	 * @param expectedEventCount
-	 * @throws IOException
-	 */
-	private void testRetrievalCountOnServer(String pvName, int expectedEventCount) throws IOException { 
-		 RawDataRetrievalAsEventStream rawDataRetrieval = new RawDataRetrievalAsEventStream("http://localhost:" + ConfigServiceForTests.RETRIEVAL_TEST_PORT+ "/retrieval/data/getData.raw");
+    private static String statusOf(String pv) {
+        JSONArray status = GetUrlContent.getURLContentAsJSONArray(MGMT + "getPVStatus?pv=" + enc(pv));
+        if (status == null || status.isEmpty()) {
+            return "(absent)";
+        }
+        return String.valueOf(((JSONObject) status.get(0)).get("status"));
+    }
+
+    @Test
+    public void testVALNoVALTest() throws Exception {
+        String pvNameToArchive1 = "UnitTestNoNamingConvention:sine";
+        String pvNameToArchive2 = "UnitTestNoNamingConvention:cosine.VAL";
+
+        // Wait for the appliance to accept archive requests, then submit both PVs.
+        Awaitility.await()
+                .atMost(Duration.ofMinutes(2))
+                .pollInterval(Duration.ofSeconds(5))
+                .ignoreExceptions()
+                .until(() -> GetUrlContent.getURLContentAsJSONArray(MGMT + "archivePV?pv=" + enc(pvNameToArchive1)) != null);
+        GetUrlContent.getURLContentAsJSONArray(MGMT + "archivePV?pv=" + enc(pvNameToArchive2));
+
+        // Both must reach "Being archived".
+        for (String pv : new String[] {pvNameToArchive1, pvNameToArchive2}) {
+            Awaitility.await()
+                    .atMost(Duration.ofMinutes(5))
+                    .pollInterval(Duration.ofSeconds(10))
+                    .ignoreExceptions()
+                    .until(() -> "Being archived".equals(statusOf(pv)));
+        }
+
+        // Wait for samples to accumulate, then confirm data is retrievable under the bare name and .VAL.
+        Awaitility.await()
+                .atMost(Duration.ofMinutes(3))
+                .pollInterval(Duration.ofSeconds(10))
+                .ignoreExceptions()
+                .until(() -> countRetrievedEvents(pvNameToArchive1) > 55);
+        Assertions.assertTrue(
+                countRetrievedEvents(pvNameToArchive1 + ".VAL") > 55,
+                "Expecting retrievable data for the .VAL form of " + pvNameToArchive1);
+    }
+
+    /**
+     * Retrieve data for the PV and return the event count, asserting that timestamps are in order.
+     */
+    private int countRetrievedEvents(String pvName) throws Exception {
+        RawDataRetrievalAsEventStream rawDataRetrieval = new RawDataRetrievalAsEventStream(
+                "http://localhost:" + ConfigServiceForTests.RETRIEVAL_TEST_PORT + "/retrieval/data/getData.raw");
         Instant end = TimeUtils.plusDays(TimeUtils.now(), 3);
         Instant start = TimeUtils.minusDays(end, 6);
-		 try(EventStream stream = rawDataRetrieval.getDataForPVS(new String[] { pvName}, start, end, null)) {
-			 long previousEpochSeconds = 0;
-			 int eventCount = 0;
-
-			 // We are making sure that the stream we get back has times in sequential order...
-			 if(stream != null) {
-				 for(Event e : stream) {
-					 long actualSeconds = e.getEpochSeconds();
-					 Assertions.assertTrue(actualSeconds >= previousEpochSeconds);
-					 previousEpochSeconds = actualSeconds;
-					 eventCount++;
-				 }
-			 }
-
-			 logger.info("Got " + eventCount + " event for pv " + pvName);
-			 Assertions.assertTrue(eventCount > expectedEventCount, "When asking for data using " + pvName + ", event count is incorrect We got " + eventCount + " and we were expecting at least " + expectedEventCount);
-		 }
-	}
-
+        try (EventStream stream = rawDataRetrieval.getDataForPVS(new String[] {pvName}, start, end, null)) {
+            long previousEpochSeconds = 0;
+            int eventCount = 0;
+            if (stream != null) {
+                for (Event e : stream) {
+                    long actualSeconds = e.getEpochSeconds();
+                    Assertions.assertTrue(actualSeconds >= previousEpochSeconds);
+                    previousEpochSeconds = actualSeconds;
+                    eventCount++;
+                }
+            }
+            logger.info("Got " + eventCount + " event for pv " + pvName);
+            return eventCount;
+        }
+    }
 }
